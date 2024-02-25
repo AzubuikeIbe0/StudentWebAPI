d65ac4c59f78454d6d93e379e7d67dd8f00cae30
ai-dev-official <ai.codespro@gmail.com>
2024-02-25 21:54:42 +0000

    Add project files.

---
+++ Models/StudentContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace StudentWebAPI.Models;
+
+public partial class StudentContext : DbContext
+{
+    public StudentContext()
+    {
+    }
+
+    public StudentContext(DbContextOptions<StudentContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=.\\SQLExpress; Database=Student; TrustServerCertificate=True; Trusted_Connection=True;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Student__3214EC07F4C3880B");
+
+            entity.ToTable("Student");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Country)
+                .HasMaxLength(55)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.RollNumber)
+                .HasMaxLength(10)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
